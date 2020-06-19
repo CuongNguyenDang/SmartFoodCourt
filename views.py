65cfac2480afa476369cb0aedfec8a537e906bd5
quangduy1810 <67039927+quangduy1810@users.noreply.github.com>
2020-06-18 23:00:16 -0700

    stall's order
    use this for Ordersetting

--- views.py
+++ views.py
@@ -1,19 +1,20 @@
 """
 Routes and views for the flask application.
 """
-
 from datetime import datetime
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request
 from FoodCourt import app
 import os
+#import Process
 import webbrowser
-from Controller import *
-from Model import Bill
-from momo import getUrl
+import Controller
+
 
 #Route
 #_____________________________________________________________________________
-@app.route('/', methods = ["GET","POST"])
+@app.route('/')
+@app.route('/home')
+@app.route('/index')
 def home():
     """Renders the home page."""
     return render_template(
@@ -22,20 +23,28 @@ def home():
         year=datetime.now().year,
     )
 
-@app.route('/index')
-@app.route('/home')
-def index():
-    return redirect(url_for('home'))
-
+@app.route('/menu')
+def menu():
+    """Renders the menu page."""
 @app.route('/account')
 def account():
     """Renders the account page."""
-    return render_template(
-        'account.html',
-        # title='Menu Page',
-        year=datetime.now().year,
-    )
+    # return render_template(
+    #     'menu.html',
+    #     'account.html',
+    #     # title='Menu Page',
+    #     year=datetime.now().year,
+    # )
 
+# @app.route('/about')
+# def about():
+#     """Renders the about page."""
+#     return render_template(
+#         'about.html',
+#         title='About',
+#         year=datetime.now().year,
+#         message='Your application description page.'
+#     )
 @app.route('/order')
 def order():
     """Renders the order page."""
@@ -47,35 +56,94 @@ def order():
 
 
 
-@app.route("/pay" , methods=['GET', 'POST'])
-def pay():
-    bill = Bill()
-    view = PayView()
-    c = PayByMachine(bill,None,None,view)
-    c.startPay()
-    c.pay(bill)
-    c.saveLog()
-    c.finishPay()
-    return render_template("index.html")
+@app.route("/test" , methods=['GET', 'POST'])
+def test():
+    select = request.form.get('comp_select')
+    return(str(select)) # just to see what select is
 
+
+@app.route('/select', methods = ['POST'])
+def pay():
+    # webbrowser.open_new('http://127.0.0.1:5000/table')
+    v = PayView()
+    controller = Controller.Payment(None,None,None,v)
+    controller.startPay()
+    return render_template('index.html')
+#Duy's part_________________________________________________________________________
+@app.route('/stallorder')
+def stallorder():
+    """Renders the order page."""
+    return render_template(
+        'stallorder.html',
+        # title='Menu Page',
+        year=datetime.now().year,
+        state= "Chua nhan"
+        
+    )
+@app.route('/status')
+def status():
+    """Renders the order page."""
+    return render_template(
+        'status.html',
+        # title='Menu Page',
+        year=datetime.now().year,
+    )
+@app.route('/detailorder')
+def detailorder():
+    """Renders the order page."""
+    return render_template(
+        'detailorder.html',
+        # title='Menu Page',
+        year=datetime.now().year,
+    )
+#Duy's end_________________________________________________________________________
 #View
 #___________________________________________________________________________________
 
 class MainUI:
-    def __init__(self):
-        self.payView = PayView()
-        self.orderView = OrderView()
+    pass #Do nothing
 
 class PayView:
     def showPaymentUI(self):
-        return redirect(url_for('home'))
+        return render_template('pay.html')
     def showResult(self):
         pass
     def showThirdServiceUI(self):
         pass
-    def showQRCode(self,qrUrl):
-        webbrowser.open_new_tab(qrUrl)
+    def showQRCode(self,qr):
+        return render_template(
+            'pay.html',
+            qrCode = qr
+        )
 
-class OrderView:
-    def showOrderUI(self):
-        return render_template("index.html")
+# @app.route('/get-text', methods=['POST'])
+# def foo():
+#     # name = request.form['test']
+#     toan = float(request.form['toan'])
+#     van = float(request.form['van'])
+#     li = float(request.form['li'])
+#     hoa = float(request.form['hoa'])
+#     sinh = float(request.form['sinh'])
+#     su = float(request.form['su'])
+#     dia = float(request.form['dia'])
+#     gdcd = float(request.form['gdcd'])
+#     anh = float(request.form['anh'])
+
+#     #Process.Prediction(toan, van, li, hoa, sinh, su, dia, gdcd, anh)
+
+#     return render_template(
+#         'result.html',
+#         title='Home Page',
+#         year=datetime.now().year,
+#         toan = float(request.form['toan']),
+#         van = float(request.form['van']),
+#         li = float(request.form['li']),
+#         hoa = float(request.form['hoa']),
+#         sinh = float(request.form['sinh']),
+#         su = float(request.form['su']),
+#         dia = float(request.form['dia']),
+#         gdcd = float(request.form['gdcd']),
+#         anh = float(request.form['anh'])
+#     )
+class OderView:
+    pass #Do nothing
