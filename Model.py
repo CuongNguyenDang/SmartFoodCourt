41d1477077f76f7397ba336cbc704fe29fc54cde
quangduy1810 <67039927+quangduy1810@users.noreply.github.com>
2020-06-21 10:25:46 -0700

    basic use of stallorder

--- Model.py
+++ Model.py
@@ -1,5 +1,3 @@
-import uuid
-
 
 class Customer:
     def __init__(self,mode):
@@ -22,10 +20,24 @@ class Account(Customer):
         return 0
     def recharge(self, money):
         return 0
+#Duy's part_____________________________________
+class FoodOrder:
+    def __init__(self,orderID,cusname,name=None, quantity=None, state=None):
+        self.orderID = orderID
+        self.cusname = cusname
+        self.name=[]
+        self.quantity=[]
+        self.state=[]
+    def addFood(self,name,quantity,state):
+        self.name.append(name)
+        self.quantity.append(quantity)
+        self.state.append(state)
+    def deleteFood(self,name):
+        i=0
+        for x in self.name:
+            if x==name:
+                self.quantity[i]=0
+                self.state[i] ="Da xoa"
+            i+=1
+#Enn Duy's part__________________________________
 
-class Bill:
-    def __init__(self,foods = []):
-        self.id = str(uuid.uuid1())
-        self.foods = foods
-        self.cost = 2000
-        
--- views.py
+++ views.py
@@ -8,7 +8,7 @@ import os
 #import Process
 import webbrowser
 import Controller
-
+from Model import FoodOrder
 
 #Route
 #_____________________________________________________________________________
@@ -70,24 +70,43 @@ def pay():
     controller.startPay()
     return render_template('index.html')
 #Duy's part_________________________________________________________________________
+Foodlist =[["Ga",1,"Chua lam"],
+      ["Vit",2,"Chua lam"],
+      ["Bo",1,"Chua lam"],
+      ["Ca",2,"Chua lam"],
+      ["Heo",1,"Chua lam"],
+      ["Mi",2,"Chua lam"],
+      ["Com",2,"Chua lam"],
+      ["Hu tieu",1,"Chua lam"],
+      ["Bun",1,"Chua lam"],
+      ["Coca",1,"Chua lam"],
+      ["Pepsi",2,"Chua lam"],
+      ["7up",3,"Chua lam"]]
+#customer1
+or1=[Foodlist[1],Foodlist[3],Foodlist[5],Foodlist[7],Foodlist[9],Foodlist[11]]
+ord1=FoodOrder("0001","Phuong Bao Tran") 
+for food in or1:
+    ord1.addFood(food[0],food[1],food[2])
+#customer2
+or2=[Foodlist[0],Foodlist[2],Foodlist[4],Foodlist[6],Foodlist[8],Foodlist[10]]
+ord2=FoodOrder("0002","Le Quang Duy") 
+for food in or2:
+    ord2.addFood(food[0],food[1],food[2])
+#views
 @app.route('/stallorder')
 def stallorder():
+    
     """Renders the order page."""
     return render_template(
         'stallorder.html',
         # title='Menu Page',
         year=datetime.now().year,
-        state= "Chua nhan"
-        
+        #customer1
+        status=["Chua nhan","Dang lam","Da xong","Da xoa"],
+        order1=ord1,
+        order2=ord2,
     )
-@app.route('/status')
-def status():
-    """Renders the order page."""
-    return render_template(
-        'status.html',
-        # title='Menu Page',
-        year=datetime.now().year,
-    )
+
 @app.route('/detailorder')
 def detailorder():
     """Renders the order page."""
@@ -95,6 +114,8 @@ def detailorder():
         'detailorder.html',
         # title='Menu Page',
         year=datetime.now().year,
+        order1=ord1,
+        order2=ord2,
     )
 #Duy's end_________________________________________________________________________
 #View
