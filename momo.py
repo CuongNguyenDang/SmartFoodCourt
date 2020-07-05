08634a416100ec3b9f4d7354ebf2a1aad0a57d48
Nguyen Dang Cuong <cuong.nguyen1605@hcmut.edu.vn>
2020-07-05 11:18:57 +0700

    query transaction status

--- momo.py
+++ momo.py
@@ -4,13 +4,12 @@ import hmac
 import hashlib
 
 import requests
-
 #parameters send to MoMo get get payUrl
 def getUrl(_amount):
 	endpoint = "https://test-payment.momo.vn/gw_payment/transactionProcessor"
 	partnerCode = "MOMOTKZV20200616"
 	accessKey = "yBMKEkDSHBNlCNu8"
-	serectkey = "TVFKWUzFlPeM1r9nQdB1T4giVML3XHjC"
+	secretKey = "TVFKWUzFlPeM1r9nQdB1T4giVML3XHjC"
 	orderInfo = "pay with MoMo"
 	returnUrl = "https://momo.vn"
 	notifyurl = "https://momo.vn"
@@ -29,7 +28,7 @@ def getUrl(_amount):
 	# print("--------------------RAW SIGNATURE----------------")
 	# print(rawSignature)
 	#signature
-	h = hmac.new(bytes(serectkey,'latin-1'), bytes(rawSignature,'latin-1'), hashlib.sha256)
+	h = hmac.new(bytes(secretKey,'latin-1'), bytes(rawSignature,'latin-1'), hashlib.sha256)
 	signature = h.hexdigest()
 	# print("--------------------SIGNATURE----------------")
 	# print(signature)
@@ -61,4 +60,41 @@ def getUrl(_amount):
 
 	# print("--------------------JSON response----------------\n")
 	# print(response)
-	return response['payUrl']
+	return (response['payUrl'],orderId)
+
+def getResult(_orderId):
+	endpoint = "https://test-payment.momo.vn/gw_payment/transactionProcessor"
+	partnerCode = "MOMOTKZV20200616"
+	accessKey = "yBMKEkDSHBNlCNu8"
+	secretKey = "TVFKWUzFlPeM1r9nQdB1T4giVML3XHjC"
+	orderInfo = "pay with MoMo"
+	returnUrl = "https://momo.vn"
+	notifyurl = "https://momo.vn"
+	orderId = str(_orderId)
+	requestId = "MM1540456472575"
+	requestType = "transactionStatus"
+	extraData = "abc" #pass empty value if your merchant does not have stores else merchantName=[storeName]; merchantId=[storeId] to identify a transaction map with a physical store
+
+	#before sign HMAC SHA256 with format
+	#partnerCode=$partnerCode&accessKey=$accessKey&requestId=$requestId&orderId=$orderId&requestType=$requestType,$secretKey
+
+	rawSignature = "partnerCode="+partnerCode+"&accessKey="+accessKey+"&requestId="+requestId+"&orderId="+orderId+"&requestType="+requestType#+","+secretKey
+	#signature
+	h = hmac.new(bytes(secretKey,'latin-1'), bytes(rawSignature,'latin-1'), hashlib.sha256)
+	signature = h.hexdigest()
+	#json object send to MoMo endpoint
+	data = {
+			'partnerCode' : partnerCode,
+			'accessKey' : accessKey,
+			'requestId' : requestId,
+			'orderId' : orderId,
+			'requestType' : requestType,
+			'signature' : signature
+	}
+	data = json.dumps(data)
+
+	headers = {'Content-Type': 'application/json'}
+
+	response = requests.post(endpoint,data=data,headers=headers)
+	response = response.json()
+	return response['message']
