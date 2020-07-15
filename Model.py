e9e65c707265e540e201de5534cabb28897bbd56
quangduy1810 <67039927+quangduy1810@users.noreply.github.com>
2020-07-15 02:37:49 -0700

    fix statuschange

--- Model.py
+++ Model.py
@@ -21,23 +21,115 @@ class Account(Customer):
     def recharge(self, money):
         return 0
 #Duy's part_____________________________________
+status=["Chưa làm","Đang làm","Đã xong","Xóa"]
+class OrderList:
+    def __init__(self,order): 
+        self.order=[]
+        self.max=0
+        if order is not None:
+            order.orderID = self.max+1
+            self.order.append(order)
+            self.max=1
+    def push(self, order):
+        order.orderID = self.max+1
+        self.max+=1
+        self.order.append(order)
+    def findbyName(self, name):
+        result = []
+        for ords in self.order:
+            if ords.name == name:
+                result.append(ords)
+                return result
+    def removeFood(self, ID,fID):
+        for ords in self.order:
+            if ords.orderID == ID:
+                x=0
+                while ords.food[x].foodID< ords.max:
+                    if ords.food[x].foodID==fID:
+                        a=x
+                        while ords.food[a].foodID <ords.max:
+                            ords.food[a]= ords.food[a+1]
+                            a+=1
+                        del ords.food[a]
+                    x+=1
+    def changestatus(self,ID,fID,foodstatus):
+        i= int(foodstatus)
+        def use0():
+            return status[0]
+        def use1():
+            return status[1]
+        def use2():
+            return status[2]
+        def use3():
+            self.removeFood(ID,fID)
+        switcher={
+                0:use0,
+                1:use1,
+                2:use2,
+                3:use3
+            }
+        for ords in self.order:
+            if ords.orderID== ID:
+                for f in ords.food:
+                    if f.foodID== fID:
+                        func= switcher.get(i)
+                        if i!=3:
+                            f.state=func()
+                            print(f.state)
+                        else:
+                            func()
+                return ords
+    def clear(self):
+        self.__init__(None)
 class FoodOrder:
-    def __init__(self,orderID,cusname,name=None, quantity=None, state=None):
-        self.orderID = orderID
-        self.cusname = cusname
-        self.name=[]
-        self.quantity=[]
-        self.state=[]
-    def addFood(self,name,quantity,state):
-        self.name.append(name)
-        self.quantity.append(quantity)
-        self.state.append(state)
-    def deleteFood(self,name):
-        i=0
-        for x in self.name:
-            if x==name:
-                self.quantity[i]=0
-                self.state[i] ="Da xoa"
-            i+=1
+    def __init__(self,cusname,food):
+        self.food=[]
+        self.max=0
+        if food is not None:
+            food.foodID=1
+            self.food.append(food)
+            self.max = food.foodID
+        self.name = cusname
+        self.orderID=0
+    def addFood(self,food):
+        food.foodID= self.max+1
+        self.max= food.foodID
+        self.food.append(food)
+
+class Food:
+    def __init__(self,fname,quantity):
+        self.name= fname
+        self.quantity=quantity
+        self.state= status[0]
+        self.foodID=0
+Foodlist =[["Gà",1],
+      ["Vịt",2],
+      ["Bò",1],
+      ["Cá",2],
+      ["Heo",1],
+      ["Mì",2],
+      ["Cơm",2],
+      ["Hủ tiếu",1],
+      ["Bún",1],
+      ["Coca",1],
+      ["Pepsi",2],
+      ["7up",3]]
+
+#customer1
+or1=[Foodlist[1],Foodlist[3],Foodlist[5],Foodlist[7],Foodlist[9],Foodlist[11]]
+ord1=FoodOrder("Phương Bảo Trân",None)
+for food in or1:
+    f=Food(food[0],food[1])
+    ord1.addFood(f)
+#customer2
+or2=[Foodlist[0],Foodlist[2],Foodlist[4],Foodlist[6],Foodlist[8],Foodlist[10]]
+ord2=FoodOrder("Lê Quang Duy",None) 
+for food in or2:
+    f=Food(food[0],food[1])
+    ord2.addFood(f)
+OrdList= OrderList(ord1)
+OrdList.push(ord2)
+
+
 #Enn Duy's part__________________________________
 
--- views.py
+++ views.py
@@ -1,16 +1,16 @@
 """
 Routes and views for the flask application.
 """
-from datetime import datetime
-from flask import render_template, request
-from FoodCourt import app
-import os
+
 #import Process
 import webbrowser
 import Controller
-from Model import FoodOrder
-
-#Route
+from Model import OrdList, status
+from datetime import datetime
+from flask import render_template, request, redirect, url_for
+from FoodCourt import app
+import os
+import glob
 #_____________________________________________________________________________
 @app.route('/')
 @app.route('/home')
@@ -70,28 +70,7 @@ def pay():
     controller.startPay()
     return render_template('index.html')
 #Duy's part_________________________________________________________________________
-Foodlist =[["Ga",1,"Chua lam"],
-      ["Vit",2,"Chua lam"],
-      ["Bo",1,"Chua lam"],
-      ["Ca",2,"Chua lam"],
-      ["Heo",1,"Chua lam"],
-      ["Mi",2,"Chua lam"],
-      ["Com",2,"Chua lam"],
-      ["Hu tieu",1,"Chua lam"],
-      ["Bun",1,"Chua lam"],
-      ["Coca",1,"Chua lam"],
-      ["Pepsi",2,"Chua lam"],
-      ["7up",3,"Chua lam"]]
-#customer1
-or1=[Foodlist[1],Foodlist[3],Foodlist[5],Foodlist[7],Foodlist[9],Foodlist[11]]
-ord1=FoodOrder("0001","Phuong Bao Tran") 
-for food in or1:
-    ord1.addFood(food[0],food[1],food[2])
-#customer2
-or2=[Foodlist[0],Foodlist[2],Foodlist[4],Foodlist[6],Foodlist[8],Foodlist[10]]
-ord2=FoodOrder("0002","Le Quang Duy") 
-for food in or2:
-    ord2.addFood(food[0],food[1],food[2])
+
 #views
 @app.route('/stallorder')
 def stallorder():
@@ -102,20 +81,41 @@ def stallorder():
         # title='Menu Page',
         year=datetime.now().year,
         #customer1
-        status=["Chua nhan","Dang lam","Da xong","Da xoa"],
-        order1=ord1,
-        order2=ord2,
+        orderls=OrdList,
+        limit=[0,1,2]
     )
 
+@app.route('/detailorder<name>', methods=["GET","POST"])
+def detailof(name):
+    cusorder = OrdList.findbyName(name)[0]
+    if request.method == "POST":
+        #search = request.form['search']
+        return render_template(
+            'detailorder.html',
+            order=cusorder,
+            foodstatus= [0,1,2,3],
+            status= status
+        )
+    return render_template(
+        'detailorder.html',
+        order=cusorder,
+        foodstatus=[0,1,2,3],
+        status= status
+    )
+@app.route('/changestatus<ID>,<fID>,<stt>', methods=["GET","POST"])
+def changestatus(ID,fID,stt):
+    ordered=OrdList.changestatus(int(ID),int(fID),int(stt))
+    #ordered= OrdList.findbyName("Lê Quang Duy")[0]
+    return redirect('/detailorder%s' %ordered.name)
 @app.route('/detailorder')
 def detailorder():
     """Renders the order page."""
     return render_template(
         'detailorder.html',
+        #'testdetail.html',
         # title='Menu Page',
         year=datetime.now().year,
-        order1=ord1,
-        order2=ord2,
+        orderls=OrdList    
     )
 #Duy's end_________________________________________________________________________
 #View
